20de488b7eb464c268b4210f248a60581ee2f291
anu <ananyaaamishraa12@gmail.com>
2026-06-21 18:58:12 +0530

    connected frontend with live backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "/api",
+  baseURL: "baseURL: "https://herbalhub-6xev.onrender.com/api",
 });
 
 client.interceptors.request.use((config) => {
